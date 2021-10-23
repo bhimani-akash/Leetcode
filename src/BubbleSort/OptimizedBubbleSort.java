4ab3910c28fd82992dbd99657f9e3fd6aea29d46
bhimani-akash <bhimani.a@northeastern.edu>
2021-10-23 01:04:35 -0400

    added optimized bubble sort

---
+++ src/BubbleSort/OptimizedBubbleSort.java
@@ -0,0 +1,40 @@
+package BubbleSort;
+
+public class OptimizedBubbleSort {
+
+    void bubbleSort(int[] arr, int n) {
+        int i, j, temp;
+        boolean swapped;
+
+        for (i = 0; i < n-1; i++) {
+            swapped = false;
+
+            for (j = 0; j < n-i-1; j++) {
+                if (arr[j] > arr[j+1]) {
+                    swapped = true;
+                    temp = arr[j];
+                    arr[j] = arr[j+1];
+                    arr[j+1] = temp;
+                }
+            }
+
+            if (!swapped) break;
+        }
+    }
+
+    void displayArray(int[] arr, int n) {
+        for (int i = 0; i < n; i++) {
+            System.out.print(arr[i] + " ");
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {100, 123, 252, 127, 212, 101, 900};
+        int n = arr.length;
+
+        OptimizedBubbleSort sort = new OptimizedBubbleSort();
+        sort.bubbleSort(arr, n);
+        System.out.println("Sorted Array : ");
+        sort.displayArray(arr, n);
+    }
+}

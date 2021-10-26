cd595d6a908805dd7ca0473f92dd2863a025be54
bhimani-akash <bhimani.a@northeastern.edu>
2021-10-26 17:23:59 -0400

    added 234. Palindrome Linked List

---
+++ src/LinkedList/_234.java
@@ -0,0 +1,37 @@
+/*
+Given the head of a singly linked list, return true if it is a palindrome.
+
+Example 1:
+Input: head = [1,2,2,1]
+Output: true
+
+Example 2:
+Input: head = [1,2]
+Output: false
+
+Constraints:
+The number of nodes in the list is in the range [1, 105].
+0 <= Node.val <= 9
+ */
+
+package LinkedList;
+
+public class _234 {
+    public boolean isPalindrome(ListNode head) {
+        Stack<ListNode> reverse = new Stack<ListNode>();
+        ListNode temp = head;
+
+        while(temp != null){
+            reverse.push(temp);
+            temp = temp.next;
+        }
+
+        while(head !=null && !reverse.isEmpty()){
+
+            if(reverse.pop().val != head.val) return false;
+            else head = head.next;
+
+        }
+        return true;
+    }
+}

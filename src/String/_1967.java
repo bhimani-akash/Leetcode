487c02a8deb13b093d46edf8f5796a7407a417b4
bhimani-akash <bhimani.a@northeastern.edu>
2021-11-04 16:26:40 -0400

    added 1967. Number of Strings That Appear as Substrings in Word

---
+++ src/String/_1967.java
@@ -0,0 +1,14 @@
+package String;
+
+public class _1967 {
+    public int numOfStrings(String[] patterns, String word) {
+        int count = 0;
+
+        for (String pattern : patterns) {
+            if (word.contains(pattern))
+                count++;
+        }
+
+        return count;
+    }
+}

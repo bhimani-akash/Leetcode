26441fa53ad1e71167eb1db46a2b324abe0e5f8a
bhimani-akash <bhimani.a@northeastern.edu>
2021-11-08 19:11:31 -0500

    added 942. DI String Match

---
+++ src/String/_942.java
@@ -0,0 +1,41 @@
+/*
+A permutation perm of n + 1 integers of all the integers in the range [0, n] can be represented as a
+string s of length n where:
+s[i] == 'I' if perm[i] < perm[i + 1], and
+s[i] == 'D' if perm[i] > perm[i + 1].
+Given a string s, reconstruct the permutation perm and return it. If there are multiple valid
+permutations perm, return any of them.
+
+Example 1:
+Input: s = "IDID"
+Output: [0,4,1,3,2]
+
+Example 2:
+Input: s = "III"
+Output: [0,1,2,3]
+
+Example 3:
+Input: s = "DDI"
+Output: [3,2,0,1]
+
+Constraints:
+1 <= s.length <= 105
+s[i] is either 'I' or 'D'.
+ */
+
+package String;
+
+public class _942 {
+    public int[] diStringMatch(String S) {
+        int n = S.length();
+        int left = 0;
+        int right = n;
+        int[] result = new int[n+1];
+
+        for (int i = 0; i < n;i++)
+            result[i] = S.charAt(i) == 'I' ? left++ : right--;
+
+        result[n] = left;
+        return result;
+    }
+}

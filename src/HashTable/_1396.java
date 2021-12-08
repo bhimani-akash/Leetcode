6c5ae18b96865c12a240dcffb00f470742d648a6
bhimani-akash <bhimani.a@northeastern.edu>
2021-12-08 11:35:43 -0500

    added 349. Intersection of Two Arrays

--- src/HashTable/_1396.java
+++ src/HashTable/_1396.java
@@ -83,30 +83,29 @@ package HashTable;
 import java.util.HashMap;
 
 public class _1396 {
-    class UndergroundSystem {
-
-        HashMap<String, Pair<Integer, Integer>> checkoutMap = new HashMap<>();
-        HashMap<Integer, Pair<String, Integer>> checkInMap = new HashMap<>();
-
-        public UndergroundSystem() { }
-
-        public void checkIn(int id, String stationName, int t) {
-            checkInMap.put(id, new Pair<>(stationName, t));
-        }
-
-        public void checkOut(int id, String stationName, int t) {
-            Pair<String, Integer> checkIn = checkInMap.get(id);
-            String route = checkIn.getKey() + "_" + stationName;
-            int totalTime = t - checkIn.getValue();
-            Pair<Integer, Integer> checkout = checkoutMap.getOrDefault(route, new Pair<>(0, 0));
-            checkoutMap.put(route, new Pair<>(checkout.getKey() + totalTime, checkout.getValue() + 1));
-        }
-
-        public double getAverageTime(String startStation, String endStation) {
-            String route = startStation + "_" + endStation;
-            Pair<Integer, Integer> checkout = checkoutMap.get(route);
-            return (double) checkout.getKey() / checkout.getValue();
-        }
-    }
-
+//    class UndergroundSystem {
+//
+//        HashMap<String, Pair<Integer, Integer>> checkoutMap = new HashMap<>();
+//        HashMap<Integer, Pair<String, Integer>> checkInMap = new HashMap<>();
+//
+//        public UndergroundSystem() { }
+//
+//        public void checkIn(int id, String stationName, int t) {
+//            checkInMap.put(id, new Pair<>(stationName, t));
+//        }
+//
+//        public void checkOut(int id, String stationName, int t) {
+//            Pair<String, Integer> checkIn = checkInMap.get(id);
+//            String route = checkIn.getKey() + "_" + stationName;
+//            int totalTime = t - checkIn.getValue();
+//            Pair<Integer, Integer> checkout = checkoutMap.getOrDefault(route, new Pair<>(0, 0));
+//            checkoutMap.put(route, new Pair<>(checkout.getKey() + totalTime, checkout.getValue() + 1));
+//        }
+//
+//        public double getAverageTime(String startStation, String endStation) {
+//            String route = startStation + "_" + endStation;
+//            Pair<Integer, Integer> checkout = checkoutMap.get(route);
+//            return (double) checkout.getKey() / checkout.getValue();
+//        }
+//    }
 }

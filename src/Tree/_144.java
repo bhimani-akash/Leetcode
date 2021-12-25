7e8f804adc332c0dc47bad9984970a4b7336b622
bhimani-akash <bhimani.a@northeastern.edu>
2021-12-25 11:27:48 -0500

    added 144. Binary Tree Preorder Traversal

---
+++ src/Tree/_144.java
@@ -0,0 +1,41 @@
+/*
+Given the root of a binary tree, return the preorder traversal of its nodes' values.
+
+Example 1:
+Input: root = [1,null,2,3]
+Output: [1,2,3]
+
+Example 2:
+Input: root = []
+Output: []
+
+Example 3:
+Input: root = [1]
+Output: [1]
+
+Constraints:
+The number of nodes in the tree is in the range [0, 100].
+-100 <= Node.val <= 100
+ */
+package Tree;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class _144 {
+//    public List<Integer> preorderTraversal(TreeNode root) {
+//        List<Integer> values = new ArrayList<>();
+//
+//        preorder(root, values);
+//
+//        return values;
+//    }
+//
+//    public void preorder(TreeNode root, List<Integer> values) {
+//        if (root != null) {
+//            values.add(root.val);
+//            preorder(root.left, values);
+//            preorder(root.right, values);
+//        }
+//    }
+}

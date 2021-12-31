558a8a7a4aec7c257191aa05cbec2450d13eb56a
bhimani-akash <bhimani.a@northeastern.edu>
2021-12-31 17:33:37 -0500

    added 404. Sum of Left Leaves

---
+++ src/Tree/_404.java
@@ -0,0 +1,35 @@
+/*
+Given the root of a binary tree, return the sum of all left leaves.
+
+Example 1:
+Input: root = [3,9,20,null,null,15,7]
+Output: 24
+Explanation: There are two left leaves in the binary tree, with values 9 and 15 respectively.
+
+Example 2:
+Input: root = [1]
+Output: 0
+
+Constraints:
+The number of nodes in the tree is in the range [1, 1000].
+-1000 <= Node.val <= 1000
+ */
+package Tree;
+
+public class _404 {
+//    public int sumOfLeftLeaves(TreeNode root) {
+//        if (root == null)
+//            return 0;
+//
+//        int sum = 0;
+//
+//        if (root.left != null && root.left.left == null && root.left.right == null) {
+//            sum += root.left.val;
+//        } else {
+//            sum += sumOfLeftLeaves(root.left);
+//        }
+//
+//        sum += sumOfLeftLeaves(root.right);
+//        return sum;
+//    }
+}

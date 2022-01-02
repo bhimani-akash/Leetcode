26a43fa43f77985708aa14462bdd6118a7b2e0a8
bhimani-akash <bhimani.a@northeastern.edu>
2022-01-02 16:38:09 -0500

    added 1305. All Elements in Two Binary Search Trees

---
+++ src/Tree/_1305.java
@@ -0,0 +1,41 @@
+/*
+Given two binary search trees root1 and root2, return a list containing all the integers from both trees sorted in
+ascending order.
+
+Example 1:
+Input: root1 = [2,1,4], root2 = [1,0,3]
+Output: [0,1,1,2,3,4]
+
+Example 2:
+Input: root1 = [1,null,8], root2 = [8,1]
+Output: [1,1,8,8]
+
+Constraints:
+The number of nodes in each tree is in the range [0, 5000].
+-105 <= Node.val <= 105
+ */
+package Tree;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class _1305 {
+//    List<Integer> res = new ArrayList<>();
+//
+//    public List<Integer> getAllElements(TreeNode root1, TreeNode root2) {
+//        inorder(root1);
+//        inorder(root2);
+//
+//        Collections.sort(res);
+//
+//        return res;
+//    }
+//
+//    public void inorder(TreeNode root) {
+//        if (root != null) {
+//            inorder(root.left);
+//            res.add(root.val);
+//            inorder(root.right);
+//        }
+//    }
+}

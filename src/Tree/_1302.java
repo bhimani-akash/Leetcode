9fdb9adca149ffff2dae68add5b7cf77a505e0d5
bhimani-akash <bhimani.a@northeastern.edu>
2022-01-01 19:46:51 -0500

    added 1302. Deepest Leaves Sum

---
+++ src/Tree/_1302.java
@@ -0,0 +1,39 @@
+/*
+Given the root of a binary tree, return the sum of values of its deepest leaves.
+
+Example 1:
+Input: root = [1,2,3,4,5,null,6,7,null,null,null,null,8]
+Output: 15
+
+Example 2:
+Input: root = [6,7,8,2,7,1,3,9,null,1,4,null,null,null,5]
+Output: 19
+
+Constraints:
+The number of nodes in the tree is in the range [1, 104].
+1 <= Node.val <= 100
+ */
+package Tree;
+
+public class _1302 {
+//    public int deepestLeavesSum(TreeNode root) {
+//        int maxDepth = getMaxDepth(root);
+//        return getSum(root, 1, maxDepth);
+//    }
+//
+//    public int getSum(TreeNode node, int depth, int maxDepth) {
+//        if (node == null)
+//            return 0;
+//
+//        if (depth == maxDepth)
+//            return node.val;
+//
+//        return getSum(node.left, depth+1, maxDepth) + getSum(node.right, depth+1, maxDepth);
+//    }
+//
+//    public int getMaxDepth(TreeNode node) {
+//        if (node == null)
+//            return 0;
+//        return Math.max(getMaxDepth(node.left), getMaxDepth(node.right)) + 1;
+//    }
+}

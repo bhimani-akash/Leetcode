0ef797a4209faaf92b5f7cfe3984a151fd3acf29
bhimani-akash <bhimani.a@northeastern.edu>
2022-08-17 14:18:24 -0400

    added 804. Unique Morse Code Words

---
+++ src/Array/_804.java
@@ -0,0 +1,83 @@
+/*
+International Morse Code defines a standard encoding where each letter is mapped to a series of dots and dashes,
+as follows:
+'a' maps to ".-",
+'b' maps to "-...",
+'c' maps to "-.-.", and so on.
+For convenience, the full table for the 26 letters of the English alphabet is given below:
+[".-","-...","-.-.","-..",".","..-.","--.","....","..",".---","-.-",".-..","--","-.","---",".--.","--.-",".-.",
+"...","-","..-","...-",".--","-..-","-.--","--.."]
+Given an array of strings words where each word can be written as a concatenation of the Morse code of each letter.
+For example, "cab" can be written as "-.-..--...", which is the concatenation of "-.-.", ".-", and "-...". We will
+call such a concatenation the transformation of a word.
+Return the number of different transformations among all words we have.
+
+Example 1:
+Input: words = ["gin","zen","gig","msg"]
+Output: 2
+Explanation: The transformation of each word is:
+"gin" -> "--...-."
+"zen" -> "--...-."
+"gig" -> "--...--."
+"msg" -> "--...--."
+There are 2 different transformations: "--...-." and "--...--.".
+
+Example 2:
+Input: words = ["a"]
+Output: 1
+
+Constraints:
+1 <= words.length <= 100
+1 <= words[i].length <= 12
+words[i] consists of lowercase English letters.
+ */
+package Array;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class _804 {
+    public int uniqueMorseRepresentations(String[] words) {
+        String[] morse = new String[]{
+                ".-",
+                "-...",
+                "-.-.",
+                "-..",
+                ".",
+                "..-.",
+                "--.",
+                "....",
+                "..",
+                ".---",
+                "-.-",
+                ".-..",
+                "--",
+                "-.",
+                "---",
+                ".--.",
+                "--.-",
+                ".-.",
+                "...",
+                "-",
+                "..-",
+                "...-",
+                ".--",
+                "-..-",
+                "-.--",
+                "--.."
+        };
+
+        Set<String> seen = new HashSet<>();
+
+        for (String word : words) {
+            StringBuilder code = new StringBuilder();
+
+            for (char c: word.toCharArray())
+                code.append(morse[c - 'a']);
+
+            seen.add(code.toString());
+        }
+
+        return seen.size();
+    }
+}
